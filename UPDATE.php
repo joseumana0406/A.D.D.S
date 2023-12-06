cc40477888519daad9306af091d757269aad29c0
unknown <bottle@OC-TEC-LU-010.superxtra.com>
2023-12-06 18:32:55 -0500

    listo

---
+++ UPDATE.php
@@ -0,0 +1,34 @@
+<html>
+    
+    <head>
+        <title>Consulta</title>
+    </head>
+    <body>
+    <center>
+        <button>
+
+<?php
+require_once('conexiondb.php');
+
+// echo "Hola ;)";
+
+$conex = new Conexion();
+$getConection = $conex->Conectar();
+
+$sql = 'SELECT * FROM SUPER_AIRLINE.CLIENTES';
+$query = $getConection->prepare($sql);
+$query->execute();
+echo "<br>";
+while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+  echo "<br>";
+  foreach ($row as $key => $value) {
+    echo $key." __ ".$value."<br>";
+  }
+}
+
+?>
+</button>
+</center>
+
+</body>
+</html>
